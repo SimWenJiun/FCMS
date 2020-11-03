c1a8b831335adf3ed1d39b052339775627dda433
Camushii <55526141+Camushii@users.noreply.github.com>
2020-11-03 14:53:53 +0800

    create button to link to payment Summary page

--- paymentConfirmation.php
+++ paymentConfirmation.php
@@ -17,6 +17,7 @@
 				<h2>Thank you for booking with us</h2>
 				</br>
 				<p>For further information, please contact us at <a href="KevinMalone@FoodEdge.com" id="emailP">KevinMalone@FoodEdge.com</a></p>
+				<button type="button" id="paymentSummary" class="btn"><a href="paymentSummary.php">View payment summary</a></button>
 				<button type="button" id="backToMenu" class="btn"><a href=".php">Back to Home Page</a></button>
 			</div>
 		</div>
