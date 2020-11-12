2e85d287da13248b4ce809d721a04d3a09d8d533
Brendan <brendanpekck@gmail.com>
2020-11-12 22:40:49 +0800

    Fix login.php

--- login.php
+++ login.php
@@ -5,17 +5,31 @@ if(count($_POST)>0) {
 	$conn = mysqli_connect("sql12.freesqldatabase.com", "sql12369317", "KGUuPpDYfu", "sql12369317");
 	$result = mysqli_query($conn,"SELECT * FROM Customers WHERE customer_username='" . $_POST["uname"] . "' and customer_password = '". $_POST["pword"]."'");
 	$row  = mysqli_fetch_array($result);
+	$manager = mysqli_query($conn,"SELECT * FROM Management WHERE username='" . $_POST["uname"] . "'");
+	$rowm  = mysqli_fetch_array($manager);
+	$operate = mysqli_query($conn,"SELECT * FROM Operation WHERE username='" . $_POST["uname"] . "'");
+	$rowo  = mysqli_fetch_array($operate);
 	if(is_array($row)) {
 		$_SESSION["id"] = $row["customer_id"];
 		$_SESSION["email"] = $row["customer_email"];
 		$_SESSION["username"] = $row["customer_username"];
 		$_SESSION["phone"] = $row["customer_phone"];
+	} else if(is_array($rowm)){
+		$_SESSION["management"] = $rowm["management_id"];
+	} else if(is_array($rowo)){
+		$_SESSION["operation"] = $rowo["operation_id"];
 	} else {
 		$message = "Invalid Username or Password!";
 	}
 }
 if(isset($_SESSION["id"])) {
 	header("Location:profile.php");
+	exit;
+} else if(isset($_SESSION["management"])) {
+	header("Location:management_home.php");
+	exit;
+} else if(isset($_SESSION["operation"])) {
+	header("Location:operation_dashboard.php");
 	exit;
 }
 ?>
