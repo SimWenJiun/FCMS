29f6797092e26ffe6b5ead86a3a1765e5278e414
shaheemsiddiqui <sha7eemsiddiqui@gmail.com>
2020-11-13 16:08:26 +0500

    bookingsubmit requires email

--- bookingsubmit.php
+++ bookingsubmit.php
@@ -37,86 +37,94 @@ session_start();
 	$orderplaceddate = date("Y-m-d");
 	$orderplacedtime = date("H:i");
 	
-	$customeremail = $_SESSION["email"];
-
-	//to connect to db
-	$dbconn = mysqli_connect("sql12.freesqldatabase.com", "sql12369317", "KGUuPpDYfu", "sql12369317");
-
-	if(!$dbconn)
+	if(isset($_SESSION["email"]))
 	{
-		die("Connection failed: " . mysqli_connect_error());
-	}
+	
+		$customeremail = $_SESSION["email"];
+		
+		//to connect to db
+		$dbconn = mysqli_connect("sql12.freesqldatabase.com", "sql12369317", "KGUuPpDYfu", "sql12369317");
 
-	$insert = mysqli_query($dbconn,
-	"INSERT INTO Orders
-	( 
-		item1name, 
-		item1price, 
-		item1quantity, 
-		item2name, 
-		item2price, 
-		item2quantity, 
-		item3name, 
-		item3price, 
-		item3quantity, 
-		item4name, 
-		item4price, 
-		item4quantity, 
-		item5name, 
-		item5price, 
-		item5quantity, 
-		item6name, 
-		item6price, 
-		item6quantity, 
-		deliverydate, 
-		deliveryaddress, 
-		totalcost, 
-		paymentstatus, 
-		customeremail,
-		orderstatus,
-		dateplaced,
-		timeplaced
-	) 
+		if(!$dbconn)
+		{
+			die("Connection failed: " . mysqli_connect_error());
+		}
 
-	VALUES
-	(
-		'$selectionname1', 
-		'$selectionprice1', 
-		'$selectionquantity1', 
-		'$selectionname2', 
-		'$selectionprice2', 
-		'$selectionquantity2', 
-		'$selectionname3', 
-		'$selectionprice3', 
-		'$selectionquantity3', 
-		'$selectionname4', 
-		'$selectionprice4', 
-		'$selectionquantity4', 
-		'$selectionname5', 
-		'$selectionprice5', 
-		'$selectionquantity5', 
-		'$selectionname6', 
-		'$selectionprice6', 
-		'$selectionquantity6', 
-		'$orderdeliverydate', 
-		'$orderdeliveryaddress', 
-		'$total',
-		'unpaid',
-		'$customeremail',
-		'pending',
-		'$orderplaceddate',
-		'$orderplacedtime'
-	)");
+		$insert = mysqli_query($dbconn,
+		"INSERT INTO Orders
+		( 
+			item1name, 
+			item1price, 
+			item1quantity, 
+			item2name, 
+			item2price, 
+			item2quantity, 
+			item3name, 
+			item3price, 
+			item3quantity, 
+			item4name, 
+			item4price, 
+			item4quantity, 
+			item5name, 
+			item5price, 
+			item5quantity, 
+			item6name, 
+			item6price, 
+			item6quantity, 
+			deliverydate, 
+			deliveryaddress, 
+			totalcost, 
+			paymentstatus, 
+			customeremail,
+			orderstatus,
+			dateplaced,
+			timeplaced
+		) 
 
-	if(!$insert)
-	{
-		echo mysqli_error();
+		VALUES
+		(
+			'$selectionname1', 
+			'$selectionprice1', 
+			'$selectionquantity1', 
+			'$selectionname2', 
+			'$selectionprice2', 
+			'$selectionquantity2', 
+			'$selectionname3', 
+			'$selectionprice3', 
+			'$selectionquantity3', 
+			'$selectionname4', 
+			'$selectionprice4', 
+			'$selectionquantity4', 
+			'$selectionname5', 
+			'$selectionprice5', 
+			'$selectionquantity5', 
+			'$selectionname6', 
+			'$selectionprice6', 
+			'$selectionquantity6', 
+			'$orderdeliverydate', 
+			'$orderdeliveryaddress', 
+			'$total',
+			'unpaid',
+			'$customeremail',
+			'pending',
+			'$orderplaceddate',
+			'$orderplacedtime'
+		)");
+
+		if(!$insert)
+		{
+			echo mysqli_error();
+		}
+		else
+		{
+			echo "Records added successfully.";
+		}
+
+		mysqli_close($dbconn);
+		header("Location:payment.php");
 	}
 	else
 	{
-		echo "Records added successfully.";
+		echo "<script>alert('Please Login To Submit Booking'); window.location = 'login.php';</script>";
 	}
-
-	mysqli_close($dbconn);
-	header("Location:payment.php");
 ?>
