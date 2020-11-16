6c9978b69fbc5dbb99fe85e335e852d13b56bd95
Derrick1112 <101214475@students.swinburne.edu.my>
2020-11-16 09:59:39 +0800

    minor changes to management header file

--- management_header.php
+++ management_header.php
@@ -4,7 +4,7 @@
 	<div id="navigation">
 		<div id="home"><a href="management_home.php">Home</a></div>
 		
-		<div id="services"><a href="orders.php">Orders</a></div>
+		<div id="order"><a href="orders.php">Orders</a></div>
 		
 		<div id="selection"><a href="customers.php">Customers</a></div>
 		
